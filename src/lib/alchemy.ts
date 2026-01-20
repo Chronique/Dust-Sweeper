be2294a37eb2cb819a40fc6b40e612b46ff2d87a
Chronique <adhi.chronique@gmail.com>
2026-01-20 16:08:19 +0700

    fix api alchemy

--- src/lib/alchemy.ts
+++ src/lib/alchemy.ts
@@ -1,10 +1,10 @@
 import { Network, Alchemy } from "alchemy-sdk";
 
-// Mengambil API Key dari Environment Variable (Vercel Dashboard)
+// Kita ambil langsung dari process.env
+// Saat deploy di Vercel, nilai ini akan otomatis diisi oleh server Vercel
 const settings = {
   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY, 
   network: Network.BASE_MAINNET,
 };
 
-// Export instance Alchemy
 export const alchemy = new Alchemy(settings);
