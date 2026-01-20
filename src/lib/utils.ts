568ade1e2d2a7b212bbec03dfbc4e73635ca7b76
Chronique <adhi.chronique@gmail.com>
2026-01-21 05:54:04 +0700

    added metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,8 @@ import { twMerge } from "tailwind-merge"
 export const METADATA = {
   name: "Nyawit Nih Orang",
   description: "Earn Eth or Usdc from dust token",
-  bannerImageUrl: 'https://i.imgur.com/2bsV8mV.png',
-  iconImageUrl: 'https://i.imgur.com/brcnijg.png',
+  bannerImageUrl: 'https://dust-sweeper-theta.vercel.app/banner.png',
+  iconImageUrl: 'https://dust-sweeper-theta.vercel.app/icon.png',
   // homeUrl: process.env.NEXT_PUBLIC_URL ?? "https://frames-v2-demo-lilac.vercel.app",
   homeUrl: "https://dust-sweeper-theta.vercel.app",
   splashBackgroundColor: "#FFFFFF"
